5613962101273a2f46fbb5b260ca83a53491555e
Arthur Valladares Nole <arthurvalladares@belcorp.biz>
2023-01-27 19:16:41 -0500

    Finish layout with MUI Grid

--- vnaj-cv/src/App.tsx
+++ vnaj-cv/src/App.tsx
@@ -1,15 +1,53 @@
 import { useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
 import vnaj_photo from './assets/vnaj.jpg'
 import './App.css'
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
-function Fullname({Firstname, Lastname}) {
-  return <h1>{Firstname} {Lastname}</h1>
+interface FullnameProps {
+  Firstname?: string;
+  Lastname?: string;
+}
+
+function Fullname({ Firstname, Lastname }: FullnameProps) {
+  return <Card variant="outlined" style={{borderRadius: "0px", height: "100%"}}>
+    <CardContent>
+      <a href="https://reactjs.org" target="_blank">
+        <img src={vnaj_photo} className="logo react" alt="React logo" />
+      </a>
+      <Typography sx={{ fontSize: 32 }} variant="h1" color="#0081B4" gutterBottom>
+        {Firstname} {Lastname}
+      </Typography>
+    </CardContent>
+  </Card>
+}
+
+interface SectionProps {
+  Name: string;
+}
+
+function Section({ Name }: SectionProps) {
+  return <Card variant="outlined" style={{borderRadius: "0px", height: "100%"}}>
+    <CardContent>
+      <Typography sx={{ fontSize: 32 }} variant="h1" color="#0081B4" gutterBottom>
+        SECTION {Name}
+      </Typography>
+      <Typography sx={{ fontSize: 14 }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor lacus at mollis dictum. Morbi vestibulum nisi ex, vitae consectetur quam viverra eu. Maecenas quis risus lacus. Phasellus blandit sagittis erat sit amet mollis. Mauris imperdiet iaculis vehicula. Mauris non tempor risus. Donec vitae ornare ante. Morbi at lobortis mauris. Donec rutrum mattis est quis consectetur. Fusce tempor diam sed turpis tempor, sed interdum turpis ultricies. Mauris vel odio elementum, suscipit turpis ac, lobortis felis. Suspendisse efficitur varius diam eu euismod. Maecenas a commodo massa.
+      </Typography>
+    </CardContent>
+  </Card>
 }
 
 function App() {
   const [cvInfo, setCvInfo] = useState(null)
 
-  
+
 
   useEffect(() => {
     fetch('/src/data/default.cv.json').then(res => res.json())
@@ -17,33 +55,52 @@ function App() {
   }, []);
 
   return (
-    <div className="App" style={{backgroundColor: '#242424'}}>
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={vnaj_photo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Fullname {...cvInfo?.PersonalInfo}/>
+    <div className="App" style={{ backgroundColor: '#242424' }}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0} direction="row"
+          justifyContent="center"
+          alignItems="stretch">
+          <Grid item xs={6}>
+            <Fullname {...cvInfo?.PersonalInfo} />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Degree' />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Profile' />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Contact' />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Experience' />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Skills' />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Studies' />
+          </Grid>
+          <Grid item xs={6}>
+            <Section Name='Soft Skills' />
+          </Grid>
+        </Grid>
+      </Box>
       <div className="card">
-        <button onClick={() => alert("Contacting...")}>
-          Contact Me
-        </button>
-      </div>
-      <div className="card">
-        <button id="pdf" onClick={() => {
+        <Button variant="outlined" id="pdf" onClick={() => {
           document.getElementById('pdf').onclick = function () {
             var cv = document.getElementById('root');
             var options = {
               margin: 1,
               filename: "vnaj.cv.pdf",
-              image: { type: "jpeg", quality: 0.98},
-              html2canvas: {scale: 2},
-              jsPDF: { unit: 'in', format: 'letter', orientation: "portrait"}
+              image: { type: "jpeg", quality: 0.98 },
+              html2canvas: { scale: 2 },
+              jsPDF: { unit: 'in', format: 'A4', orientation: "portrait" }
             };
-    
+
             html2pdf(cv, options);
           };
-        }}>PDF</button>
+        }}>PDF</Button>
       </div>
     </div>
   )
